3a41378273e473ff2cf886a176ad4c32298fbbac
vijay02 <vijayjayakumar0206@gmail.com>
2021-04-03 11:59:18 +0530

    generating otp with random numbers.

---
+++ generateOTP.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class generateOTP {
+    public static char[] generateotp(int length) {
+        String numbers = "1234567890";
+        Random random = new Random();
+        char otp[] = new char[length];
+        for (int i = 0; i < length; i++) {
+            otp[i] = numbers.charAt(random.nextInt(numbers.length()));
+        }
+        return otp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateotp(5));
+    }
+}
