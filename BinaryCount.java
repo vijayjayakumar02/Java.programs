aca1d427abf2fe4a9c7cb8b00909b1337bc71b21
vijay02 <vijayjayakumar0206@gmail.com>
2021-04-03 12:33:23 +0530

    number of 1's in a binary number

---
+++ BinaryCount.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class sample {
+    public static int binaryCount(int n) {
+        int count = 0;
+        while (n > 0) {
+            if (n % 2 == 1)
+                count++;
+            n /= 2;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = binaryCount(n);
+        System.out.println(count);
+        sc.close();
+    }
+}
